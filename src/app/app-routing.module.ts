d368a45099f1fe26971b9cf1b1b35932e4dcd9d8
Martin <boehmmartin2000@yahoo.at>
2019-05-02 09:45:13 +0200

    Homework_03

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ModelsComponent } from './models/models.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'about'},
+  { path: 'about', component: AboutComponent },
+  { path: 'models', component: ModelsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
